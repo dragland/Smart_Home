8e50e30ac77e5f682d2d54568ba773eb946af41b
Davy Ragland <dragland@stanford.edu>
2016-09-01 21:16:00 -1000

    Updated auto start script.

--- install.sh
+++ install.sh
@@ -2,15 +2,19 @@
 #Davy Ragland | dragland@stanford.edu
 #Home Automation System version 2.0 | 2016
 
-echo "Coded by Davy Ragland"
+echo "Home Automation System version 2.0"
+echo "Coded by Davy Ragland [dragland@stanford.edu]"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo "TO DO"
 echo "Setup static IP"
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 echo "Edit .bashrc"
-#hostname -I
+echo "hostname -I"
+
+echo "sudo crontab -e"
+echo "@reboot /var/www/run.sh &"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo "downloading..."
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo "Apache2"
 sudo apt-get update
 sudo apt-get install apache2 -y
@@ -19,8 +23,11 @@ echo "Updating..."
 sudo apt-get update
 sudo apt-get upgrade
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo "Setting up..."
+sudo chown -R -v pi /var/www
 sudo git clone https://github.com/dragland/Smart_Home.git temp
 sudo mv temp/* /var/www/
 sudo rm -rf temp
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~	
-echo "done"
+echo "done! Restarting now..."
+sudo shutdown -r now
--- run.sh
+++ run.sh
@@ -2,7 +2,9 @@
 #Davy Ragland | dragland@stanford.edu
 #Home Automation System version 2.0 | 2016
 
+sudo git fetch --all
+sudo git reset --hard origin/master
 until scripts/main.py; do
-	echo "main.py has failed with exit code $?. Restarting child process.." >&2
+	echo "main.py has failed with exit code $?. Restarting child process..." >&2
 	sleep 1
 done
