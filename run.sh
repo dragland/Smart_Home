0b3a7ddb2abc923cbfcefb6e65929392c496281f
Davy Ragland <dragland@stanford.edu>
2016-09-01 21:27:36 -1000

    Fix attempt number 3.

--- run.sh
+++ run.sh
@@ -2,8 +2,10 @@
 #Davy Ragland | dragland@stanford.edu
 #Home Automation System version 2.0 | 2016
 
+cd /var/www
 sudo git fetch --all
 sudo git reset --hard origin/master
+clear
 until scripts/main.py; do
 	echo "main.py has failed with exit code $?. Restarting child process..." >&2
 	sleep 1
