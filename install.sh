a5de2555eed36be3ebb62a7f105ec91f30fa0bf6
Davy Ragland <dragland@stanford.edu>
2016-09-02 16:18:25 -1000

    Integrated DHT22 temp & humidity sensor.

--- install.sh
+++ install.sh
@@ -5,19 +5,19 @@
 echo "Home Automation System version 2.0"
 echo "Coded by Davy Ragland [dragland@stanford.edu]"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo "TO DO"
-echo "Setup static IP"
-
-echo "Edit .bashrc"
-echo "hostname -I"
-
-echo "sudo crontab -e"
-echo "@reboot /var/www/run.sh &"
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo "downloading..."
+echo "Downloading..."
 echo "Apache2"
+echo "Python-dev"
 sudo apt-get update
 sudo apt-get install apache2 -y
+sudo apt-get install build-essential python-dev -y
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo "Installing..."
+echo "DHT22 temp & humidity sensor"
+sudo git clone https://github.com/adafruit/Adafruit_Python_DHT.git
+cd Adafruit_Python_DHT
+sudo python setup.py install
+cd /home/pi
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo "Updating..."
 sudo apt-get update
@@ -26,8 +26,17 @@ sudo apt-get upgrade
 echo "Setting up..."
 sudo chown -R -v pi /var/www
 sudo git clone https://github.com/dragland/Smart_Home.git temp
-sudo mv temp/* /var/www/
+sudo mv temp/* /var/www/.
 sudo rm -rf temp
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo "TO DO"
+echo "Setup static IP"
+
+echo "Edit .bashrc"
+echo "hostname -I"
+
+echo "sudo crontab -e"
+echo "@reboot /var/www/run.sh &"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~	
 echo "done! Restarting now..."
 sudo shutdown -r now
--- scripts/main.py
+++ scripts/main.py
@@ -7,10 +7,21 @@ import os
 import sys
 import time
 import datetime
+import Adafruit_DHT
+
+#Function to read data from AM2302 humidity and temperature sensor
+def read_rh_temp():
+	global rh
+	global temp_f
+	r_h, temp = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4, 5, 3)
+	if r_h and temp is not None:
+		rh = r_h
+		temp_f = temp * 1.8 + 32.0
+	time.sleep(2)
 
 #Function to print and write current data from each sensor module to state CSV
 def write_state():
-	data = str("%s" % (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+	data = str("%s,%3.1f,%3.1f" % (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), temp_f, rh))
 	state = open("html/state.txt", "w")
 	state.write(data + "\n")
 	state.close()
@@ -21,6 +32,14 @@ def write_state():
 
 #Actual Program
 print("Executing program:")
+temp_f = 70
+rh = 50
+t1 = threading.Thread(target=read_rh_temp)
+t1.start()
 while True:	
+	if t1.is_alive()is False:
+		del t1
+		t1 = threading.Thread(target=read_rh_temp)
+		t1.start()
 	write_state()
 	time.sleep(1)
