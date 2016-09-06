568ff8a152ed604dd9227c6902bf0c98c52137d9
Davy Ragland <dragland@stanford.edu>
2016-09-05 17:39:10 -1000

    Cleaned up Chart.js code.

--- html/js/grapher.js
+++ html/js/grapher.js
@@ -5,39 +5,34 @@
                            SETUP
 *********************************************************************/
 var ctx = document.getElementById("grapher");
-var myChart = new Chart(ctx, {
-    type: 'bar',
+var myLineChart = new Chart(ctx, {
+    type: 'line',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                data: [65, 59, 80, 81, 56, 55, 40],
+                label: "Data",
+                fill: true,
+                lineTension: 0.1,
+                backgroundColor: "rgba(75,192,192,0.4)",
+                borderWidth: 10,
+                borderColor: "rgba(75,192,192,1)",
+                borderCapStyle: 'round',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: "rgba(75,192,192,1)",
+                pointBackgroundColor: "#fff",
+                pointBorderWidth: 5,
+                pointRadius: 5,
+                pointHoverRadius: 10,
+                pointHitRadius: 10,
+                pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                pointHoverBorderColor: "rgba(220,220,220,1)",
+                pointHoverBorderWidth: 2,
+                spanGaps: false,
+            }
+        ]
     }
 });
