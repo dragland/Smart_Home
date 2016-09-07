a0dd1ec88e79d1073c7a7d82b5884e6ba6a210a0
Davy Ragland <dragland@stanford.edu>
2016-09-06 18:23:22 -1000

    Decomposed CGI scripts.

--- html/cgi-bin/switch.py
+++ html/cgi-bin/switch.py
@@ -5,20 +5,16 @@
 #*********************************************************************
 #                           SETUP
 #*********************************************************************
-import os
-import relayCGI
 import cgi
+import relayCGI
 
 #*********************************************************************
 #                            MAIN
 #*********************************************************************
-form = cgi.FieldStorage()
-PIN_NUMBER = form["PIN_NUMBER"].value
-
-os.system("gpio mode %s out" % PIN_NUMBER)
-
-if relayCGI.read_state(int(PIN_NUMBER)) == "1":
-	relayCGI.relay_on(int(PIN_NUMBER))
+PIN_NUMBER = relayCGI.get_value("PIN_NUMBER")
+relayCGI.set_mode(PIN_NUMBER)
+if relayCGI.read_state(PIN_NUMBER) == "1":
+	relayCGI.relay_on(PIN_NUMBER)
 else:
-	relayCGI.relay_off(int(PIN_NUMBER))
-relayCGI.header()
+	relayCGI.relay_off(PIN_NUMBER)
+relayCGI.print_blank_header()
