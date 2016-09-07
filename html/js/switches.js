e9a0a708c45186c87385b805dd327ab611dff08d
Davy Ragland <dragland@stanford.edu>
2016-09-06 18:36:53 -1000

    Simplified CGI script call to use HTTP GET as parameter.

--- html/js/switches.js
+++ html/js/switches.js
@@ -9,7 +9,7 @@ Function: switch_lights_red
 This function toggles the red lights.
 */
 function switch_lights_red() {
-	document.location = "cgi-bin/lights_red.py";
+	document.location = "cgi-bin/switch.py?PIN_NUMBER=0";
 }
 
 /*
@@ -17,7 +17,7 @@ Function: switch_lights_green
 This function toggles the green lights.
 */
 function switch_lights_green() {
-	document.location = "cgi-bin/lights_green.py";
+	document.location = "cgi-bin/switch.py?PIN_NUMBER=2";
 }
 
 /*
@@ -25,7 +25,7 @@ Function: switch_lights_blue
 This function toggles the blue lights.
 */
 function switch_lights_blue() {
-	document.location = "cgi-bin/lights_blue.py";
+	document.location = "cgi-bin/switch.py?PIN_NUMBER=3";
 }
 
 /*
@@ -33,5 +33,5 @@ Function: switch_fan
 This function toggles the fan.
 */
 function switch_fan() {
-	document.location = "cgi-bin/fan.py";
+	document.location = "cgi-bin/switch.py?PIN_NUMBER=4";
 }
