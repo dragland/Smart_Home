0de65ae313cdc253061544486f34d798491b5237
Davy Ragland <dragland@stanford.edu>
2016-09-06 18:49:33 -1000

    Bug fix attempt 2.

--- html/cgi-bin/switch.py
+++ html/cgi-bin/switch.py
@@ -5,7 +5,6 @@
 #*********************************************************************
 #                           SETUP
 #*********************************************************************
-import os
 import relayCGI
 import cgi
 
@@ -15,10 +14,9 @@ import cgi
 form = cgi.FieldStorage()
 PIN_NUMBER = form["PIN_NUMBER"].value
 
-os.system("gpio mode %s out" % PIN_NUMBER)
-
+relayCGI.set_mode(int(PIN_NUMBER))
 if relayCGI.read_state(int(PIN_NUMBER)) == "1":
 	relayCGI.relay_on(int(PIN_NUMBER))
 else:
 	relayCGI.relay_off(int(PIN_NUMBER))
-relayCGI.header()
+relayCGI.blank_header()
