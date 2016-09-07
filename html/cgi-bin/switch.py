af7820dbd3f0e02eaa8ed5dfcdcc453911732a9c
Davy Ragland <dragland@stanford.edu>
2016-09-06 19:09:23 -1000

    I fixed the bug.

--- html/cgi-bin/switch.py
+++ html/cgi-bin/switch.py
@@ -10,10 +10,10 @@ import relayCGI
 #*********************************************************************
 #                            MAIN
 #*********************************************************************
-PIN_NUMBER = relayCGI.get_value("PIN_NUMBER")
-relayCGI.set_mode(int(PIN_NUMBER))
-if relayCGI.read_state(int(PIN_NUMBER)) == "1":
-	relayCGI.relay_on(int(PIN_NUMBER))
+PIN_NUMBER = int(relayCGI.get_value("PIN_NUMBER"))
+relayCGI.set_mode(PIN_NUMBER)
+if relayCGI.read_state(PIN_NUMBER) == "1":
+	relayCGI.relay_on(PIN_NUMBER)
 else:
-	relayCGI.relay_off(int(PIN_NUMBER))
+	relayCGI.relay_off(PIN_NUMBER)
 relayCGI.blank_header()
