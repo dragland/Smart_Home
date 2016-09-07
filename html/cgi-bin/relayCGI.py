c5d4265cd61e606ca351de8c0df4689112fc8e78
Davy Ragland <dragland@stanford.edu>
2016-09-06 18:31:58 -1000

    I done goofed.

--- html/cgi-bin/relayCGI.py
+++ html/cgi-bin/relayCGI.py
@@ -12,36 +12,25 @@ import os
 #*********************************************************************
 #                          FUNCTIONS
 #*********************************************************************
-#Function: get_value
-#This function returns the value passed through the HTTP GET function.
-def get_value(KEY):
-	form = cgi.FieldStorage()
-	return = form[KEY].value
-
-#Function: set_mode
-#This function sets the mode of a relay.
-def set_mode(PIN_NUMBER):
-	os.system("gpio mode %i out" % PIN_NUMBER)
-
 #Function: read_state
 #This function reads the state of a relay.
-def read_state(PIN_NUMBER):
-	output = subprocess.check_output(["gpio read %i" % (PIN_NUMBER)], shell=True)
+def read_state(pin):
+	output = subprocess.check_output(["gpio read %i" % (pin)], shell=True)
 	return str(output)[:1]
 
 #Function: relay_on
 #This function turns on a relay.
-def relay_on(PIN_NUMBER):
-	os.system("gpio write %i 0" % (PIN_NUMBER))
+def relay_on(pin):
+	os.system("gpio write %i 0" % (pin))
 
 #Function: relay_off
 #This function turns off a relay.
-def relay_off(PIN_NUMBER):
-	os.system("gpio write %i 1" % (PIN_NUMBER))
+def relay_off(pin):
+	os.system("gpio write %i 1" % (pin))
 
 #Function: header
 #This function returns a blank HTTP packet.
-def print_blank_header():
+def header():
 	print "Status: 204 No Content"
 	print "Content-type: text/plain"
 	print ""
