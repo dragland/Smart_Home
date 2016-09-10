32e6c7ca5270b50fced7ed9355f4c05e352c8f3e
Davy Ragland <dragland@stanford.edu>
2016-09-09 18:07:44 -1000

    Created mock graph.

--- html/js/grapher.js
+++ html/js/grapher.js
@@ -21,40 +21,192 @@ var myLineChart = new Chart(ctx, {
             {
                 data: 
                 [
-                    65, 
-                    59, 
-                    80, 
-                    81, 
-                    56, 
-                    55, 
-                    40
+                    86, 
+                    86, 
+                    88, 
+                    90, 
+                    70, 
+                    92, 
+                    87
                 ],
                 label: "temp_f",
-                fill: true,
+                borderColor: "rgba(255,0,0,1)",
+                pointHoverBackgroundColor: "rgba(255,0,0,1)",
+                backgroundColor: "rgba(255,0,0,.1)",
                 lineTension: 0.1,
-                backgroundColor: "rgba(75,192,192,0.4)",
                 borderWidth: 10,
-                borderColor: "rgba(75,192,192,1)",
-                borderCapStyle: 'round',
-                borderDash: [],
-                borderDashOffset: 0.0,
-                borderJoinStyle: 'miter',
-                pointBorderColor: "rgba(75,192,192,1)",
-                pointBackgroundColor: "#fff",
                 pointBorderWidth: 5,
                 pointRadius: 5,
                 pointHoverRadius: 10,
                 pointHitRadius: 10,
-                pointHoverBackgroundColor: "rgba(75,192,192,1)",
-                pointHoverBorderColor: "rgba(220,220,220,1)",
                 pointHoverBorderWidth: 2,
-                spanGaps: false,
+                pointHoverBorderColor: "rgba(255,255,255,1)",
+                pointBackgroundColor: "rgba(255,255,255,1)",
+            },
+            {
+                data: 
+                [
+                    37, 
+                    40, 
+                    38, 
+                    39, 
+                    50, 
+                    36, 
+                    37
+                ],
+                label: "rh",
+                borderColor: "rgba(0,105,255,1)",
+                pointHoverBackgroundColor: "rgba(0,105,255,1)",
+                backgroundColor: "rgba(0,105,255,.1)",
+                lineTension: 0.1,
+                borderWidth: 10,
+                pointBorderWidth: 5,
+                pointRadius: 5,
+                pointHoverRadius: 10,
+                pointHitRadius: 10,
+                pointHoverBorderWidth: 2,
+                pointHoverBorderColor: "rgba(255,255,255,1)",
+                pointBackgroundColor: "rgba(255,255,255,1)",
+            },
+            {
+                data: 
+                [
+                    0, 
+                    20, 
+                    20, 
+                    20, 
+                    0, 
+                    0, 
+                    0
+                ],
+                label: "door",
+                borderColor: "rgba(255,242,37,1)",
+                pointHoverBackgroundColor: "rgba(255,242,37,1)",
+                backgroundColor: "rgba(255,242,37,.1)",
+                lineTension: 0.1,
+                borderWidth: 10,
+                pointBorderWidth: 5,
+                pointRadius: 5,
+                pointHoverRadius: 10,
+                pointHitRadius: 10,
+                pointHoverBorderWidth: 2,
+                pointHoverBorderColor: "rgba(255,255,255,1)",
+                pointBackgroundColor: "rgba(255,255,255,1)",
+            },
+            {
+                data: 
+                [
+                    10, 
+                    0, 
+                    0, 
+                    0, 
+                    0, 
+                    0, 
+                    0
+                ],
+                label: "lights_red",
+                borderColor: "rgba(255,0,0,1)",
+                pointHoverBackgroundColor: "rgba(255,0,0,1)",
+                backgroundColor: "rgba(255,0,0,.1)",
+                lineTension: 0.1,
+                borderWidth: 10,
+                pointBorderWidth: 5,
+                pointRadius: 5,
+                pointHoverRadius: 10,
+                pointHitRadius: 10,
+                pointHoverBorderWidth: 2,
+                pointHoverBorderColor: "rgba(255,255,255,1)",
+                pointBackgroundColor: "rgba(255,255,255,1)",
+            },
+            {
+                data: 
+                [
+                    10, 
+                    10, 
+                    10, 
+                    0, 
+                    0, 
+                    0, 
+                    0
+                ],
+                label: "lights_green",
+                borderColor: "rgba(42,200,73,1)",
+                pointHoverBackgroundColor: "rgba(42,200,73,1)",
+                backgroundColor: "rgba(42,200,73,.1)",
+                lineTension: 0.1,
+                borderWidth: 10,
+                pointBorderWidth: 5,
+                pointRadius: 5,
+                pointHoverRadius: 10,
+                pointHitRadius: 10,
+                pointHoverBorderWidth: 2,
+                pointHoverBorderColor: "rgba(255,255,255,1)",
+                pointBackgroundColor: "rgba(255,255,255,1)",
+            },
+            {
+                data: 
+                [
+                    0, 
+                    0, 
+                    0, 
+                    10, 
+                    10, 
+                    10, 
+                    10
+                ],
+                label: "lights_blue",
+                borderColor: "rgba(0,105,255,1)",
+                pointHoverBackgroundColor: "rgba(0,105,255,1)",
+                backgroundColor: "rgba(0,105,255,.1)",
+                lineTension: 0.1,
+                borderWidth: 10,
+                pointBorderWidth: 5,
+                pointRadius: 5,
+                pointHoverRadius: 10,
+                pointHitRadius: 10,
+                pointHoverBorderWidth: 2,
+                pointHoverBorderColor: "rgba(255,255,255,1)",
+                pointBackgroundColor: "rgba(255,255,255,1)",
+            },
+            {
+                data: 
+                [
+                    0, 
+                    0, 
+                    0, 
+                    30, 
+                    30, 
+                    30, 
+                    30
+                ],
+                label: "fan",
+                borderColor: "rgba(163,73,164,1)",
+                pointHoverBackgroundColor: "rgba(163,73,164,1)",
+                backgroundColor: "rgba(163,73,164,.1)",
+                lineTension: 0.1,
+                borderWidth: 10,
+                pointBorderWidth: 5,
+                pointRadius: 5,
+                pointHoverRadius: 10,
+                pointHitRadius: 10,
+                pointHoverBorderWidth: 2,
+                pointHoverBorderColor: "rgba(255,255,255,1)",
+                pointBackgroundColor: "rgba(255,255,255,1)",
             }
         ]
     },
     options: {
         legend: {
             display: false
+        },
+        scales: {
+            yAxes: [{
+                display: true,
+                ticks: {
+                    max: 100,
+                    beginAtZero: true
+                }
+            }]
         }
     }
 });
