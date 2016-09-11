1e072ec456179db0f8e020095b777d20f21ea997
Davy Ragland <dragland@stanford.edu>
2016-09-10 17:50:34 -1000

    what.

--- html/js/smartHome.js
+++ html/js/smartHome.js
@@ -79,3 +79,27 @@ This function toggles the relay CGI script.
 function switch(URL) {
 	document.location = URL;
 }
+
+/*
+Function: switch_lights_green
+This function toggles the green lights.
+*/
+function switch_lights_green() {
+	document.location = "cgi-bin/switch.py?PIN_NUMBER=2";
+}
+
+/*
+Function: switch_lights_blue
+This function toggles the blue lights.
+*/
+function switch_lights_blue() {
+	document.location = "cgi-bin/switch.py?PIN_NUMBER=3";
+}
+
+/*
+Function: switch_fan
+This function toggles the fan.
+*/
+function switch_fan() {
+	document.location = "cgi-bin/switch.py?PIN_NUMBER=4";
+}
