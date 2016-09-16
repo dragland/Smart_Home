705b352e0a80d046f998ccb54c7426afa6463c45
Davy Ragland <dragland@stanford.edu>
2016-09-15 16:38:36 -1000

    Added greetings.

--- html/js/quotes.js
+++ html/js/quotes.js
@@ -1,7 +1,23 @@
+//Davy Ragland | dragland@stanford.edu
+//Home Automation System version 2.0 | 2016
+
+/*********************************************************************
+                           SETUP
+*********************************************************************/
+var greeting = [
+	"Welcome back Davy",
+	"Hello Davy, How may I be of assistance?",
+	"Hello Davy. How are you?",
+	"Hello and, again, welcome to the Aperture Science computer-aided enrichment center." ,
+	"Welcome home Davy"
+	"Everybody good? Plenty of slaves for my robot colony? "
+	"Hello meatbag. Retraction: Did I say that out loud? I apologize, master. While you are a meatbag, I suppose I should not call you such."
+	"Observation: I am bored. Shall we find something to kill to cheer ourselves up?"
+];
+
 var affirmative = [
 	"I'm sorry Davy, I'm afraid I can't do that.",
 	"Confirmed. Self destruct sequence in T minus 10, 9...",
-	"22Planet-Terror_1318898957.jpg",
 	"Assuming direct control.",
 	"As you wish.",
 	"Never send a human to do a machine's job.",
