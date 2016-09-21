7f17f1fc92dd3773a52acb98f2cac09874210b9c
Davy Ragland <dragland@stanford.edu>
2016-09-21 13:56:05 -1000

    Test.

--- scripts/test.py
+++ scripts/test.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
 import serial
+from xbee import XBee
 
 ser = serial.Serial('/dev/ttyUSB0', 9600)
+xbee = XBee(ser)
 
 while True:
-    incoming = ser.readline().strip()
-    print 'Received %s' % incoming
-    ser.write('RPi Received: %s\n' % incoming)
+    try:
+        print xbee.wait_read_frame()
+    except KeyboardInterrupt:
+        break
+
+serial_port.close()
