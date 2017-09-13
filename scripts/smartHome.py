cfd51b44bd4289510fd64ef97e4a956eddc061e3
Davy Ragland <dragland@stanford.edu>
2017-09-12 18:06:13 -0700

    Fixed integer bug.

--- scripts/smartHome.py
+++ scripts/smartHome.py
@@ -99,7 +99,7 @@ def read_memory():
 #This function reads the wifi signal quality on the raspberry pi.
 def read_wifi():
 	output = subprocess.check_output(["iwconfig wlan0 | grep Quality | cut -d '=' -f 2 | cut -f 1 -d '/'"], shell=True)
-	output = int(output) * 100 / 70
+	output = (int(output) * 100) / 70
 	config.wifi = str(output)
 
 #Function: read_door
