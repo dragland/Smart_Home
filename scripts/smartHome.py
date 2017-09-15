c5bcb5a425d5ff58273257ba8ae9681d94d819ad
Davy Ragland <dragland@stanford.edu>
2017-09-14 23:34:28 -0700

    Edited wifi setting.

--- scripts/smartHome.py
+++ scripts/smartHome.py
@@ -97,10 +97,8 @@ def read_memory():
 #Function: read_wifi
 #This function reads the wifi signal quality on the raspberry pi.
 def read_wifi():
-	output = subprocess.check_output(["df -h | grep /dev/root | cut -d ' ' -f 14-"], shell=True)
-	state.wifi = str(output)[:2]
-	# output = subprocess.check_output(["iwconfig wlan0 | grep Quality | cut -d '=' -f 2 | cut -f 1 -d '/'"], shell=True)
-	# # state.wifi = str(output)
+	output = subprocess.check_output(["iwconfig wlan0 | grep Quality | cut -d '=' -f 2 | cut -f 1 -d '/'"], shell=True)
+	state.wifi = output
 
 #Function: read_door
 #This function reads the data from a magnetic door sensor.
