c253b136d065a7b0e8cb484376ad0fffeeab125b
Davy Ragland <dragland@stanford.edu>
2017-10-01 02:46:07 -0700

    Attempting new fix.

--- html/cgi-bin/smartHomeCGI.py
+++ html/cgi-bin/smartHomeCGI.py
@@ -74,9 +74,9 @@ def relay_off(PIN_NUMBER):
 #Function: set_leds
 #This function sets the LEDs to a specific RGB value.
 def set_leds(RED_VAL,GREEN_VAL,BLUE_VAL):
-	os.system("/var/wwww/html/cgi-bin/RGB_Driver.py -r 0 %i -g 0 %i -b 0 %i" % RED_VAL, GREEN_VAL, BLUE_VAL)
+	os.system("python RGB_Driver.py -r 0 %i -g 0 %i -b 0 %i" % RED_VAL, GREEN_VAL, BLUE_VAL)
 
 #Function: set_leds_party
 #This function turns on the party mode LED setting.
 def set_leds_party():
-	os.system("/var/wwww/html/cgi-bin/RGB_Driver.py -r 2000 4095 -g 0 1024 -b 0 0 -s 100 -d 0.1 --random --max-random-walk 100")
+	os.system("python RGB_Driver.py -r 2000 4095 -g 0 1024 -b 0 0 -s 100 -d 0.1 --random --max-random-walk 100 --time 20")
