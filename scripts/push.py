0e0275584fc7e2a183a0fd6283c8bd9abf2f7522
dragland <dragland@stanford.edu>
2018-10-03 23:28:21 -0700

    nm.

--- scripts/push.py
+++ scripts/push.py
@@ -6,7 +6,6 @@ import keys
 ip = subprocess.check_output(["hostname -I"], shell=True)
 try:
 	pb = pushbullet.Pushbullet(keys.PUSH)
-	push = pb.push_note("EVE is online at", "http://" + ip)
 	push = pb.push_sms(pb.devices[0], keys.PHONE, "EVE is online at http://" + ip)
 except:
 	print(keys.PUSH + " is incorrect...")
