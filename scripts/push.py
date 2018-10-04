d9eda6243642a48caa49039cc0a3ea2b6661a302
dragland <dragland@stanford.edu>
2018-10-04 15:07:49 -0700

    3 seconds is the magic number.

--- scripts/push.py
+++ scripts/push.py
@@ -4,28 +4,12 @@ import subprocess
 import pushbullet
 import keys
 
-import os
-import datetime
 time.sleep(3)
-filename = '/var/www/html/log.txt'
-if os.path.exists(filename):
-    append_write = 'a'
-else:
-    append_write = 'w'
-f = file = open(filename,append_write)
-f.write(str(datetime.datetime.now()))
-
 ip = subprocess.check_output(["hostname -I"], shell=True)
-f.write(ip)
 try:
 	pb = pushbullet.Pushbullet(keys.PUSH)
 	push = pb.push_sms(pb.devices[0], keys.PHONE, "EVE is online at http://" + ip)
 	print("sending...")
-	f.write("sending...\n")
 except Exception, e:
 	print("send failled...")
-	f.write("send failled...\n")
-	f.write(repr(e))
-print("done...")
-f.write("done...\n")
-f.close()
+	print(e)
