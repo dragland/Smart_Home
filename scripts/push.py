bcc5a139396004fdc7992c5a33ad5b254e24d7d3
dragland <dragland@stanford.edu>
2018-10-03 23:21:54 -0700

    OHP.

--- scripts/push.py
+++ scripts/push.py
@@ -4,9 +4,8 @@ import pushbullet
 import keys
 
 ip = subprocess.check_output(["hostname -I"], shell=True)
-
-	try:
-		pb = pushbullet.Pushbullet(keys.PUSH)
-		push = pb.push_sms(pb.devices[0], keys.PHONE, "EVE is online at http://" + ip)
-	except:
-		print(keys.PUSH + " is incorrect...")
+try:
+	pb = pushbullet.Pushbullet(keys.PUSH)
+	push = pb.push_sms(pb.devices[0], keys.PHONE, "EVE is online at http://" + ip)
+except:
+	print(keys.PUSH + " is incorrect...")
