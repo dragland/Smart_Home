3c1f818574d8a26782090f08889d69bd0ec1a9bd
dragland <dragland@stanford.edu>
2018-10-04 14:12:17 -0700

    typo.

--- scripts/push.py
+++ scripts/push.py
@@ -4,13 +4,13 @@ import pushbullet
 import keys
 
 f = file = open('/var/www/html/log.txt', 'w+')
-f.write.write(ip)
+f.write(ip)
 ip = subprocess.check_output(["hostname -I"], shell=True)
 try:
 	pb = pushbullet.Pushbullet(keys.PUSH)
 	push = pb.push_sms(pb.devices[0], keys.PHONE, "EVE is online at http://" + ip)
-	f.write.write("sending...\n")
+	f.write("sending...\n")
 except:
 	print(keys.PUSH + " is incorrect...")
-	f.write.write("send failled...\n")
-f.write.write("done...\n")
+	f.write("send failled...\n")
+f.write("done...\n")
